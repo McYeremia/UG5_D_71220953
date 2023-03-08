bdc745f9f06c2895ce3b1366fd607b4431857b83
McYeremia <117885019+McYeremia@users.noreply.github.com>
2023-03-08 19:02:37 +0700

    Add files via upload

---
+++ 3_D_71220953.py
@@ -0,0 +1,21 @@
+import math
+while True:
+    jar_awal = input('Masukkan jarak awal (meter): ')
+    if jar_awal == 'stop':
+        print('Program Dihentikan')
+        break
+    elif jar_awal == 'berhenti':
+        print('Program Dibentikan')
+        break
+    
+    sudut1 = int(input('Masukkan sudut elevasi pada menit ke-5 (derajat): '))
+    sudut2 = int(input('Masukkan sudut elevasi pada menit ke-6 (derajat): '))
+
+    tinggi_awal = float(jar_awal) * math.tan(math.radians(sudut1))
+    tinggi_akhir = float(jar_awal) * ((math.tan(math.radians(sudut2))) - (math.tan(math.radians(sudut1))))
+    selisih = tinggi_akhir * (math.tan(math.radians(sudut2)))
+
+    for i in range(1):
+        hasil1 = print(f'Ketinggian drone pada menit ke-5 adalah {tinggi_awal} meter')
+        hasil2 = print(f'Selisih ketinggian drone saat menit ke-5 dan ke-6 adalah {selisih} meter')
+
